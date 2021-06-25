a8e3da0fa5221d27541db867a18c6e564c778091
Andrew Domin <aadomin@yandex.ru>
2021-06-25 13:53:32 +0300

    word edit page

--- lib/main.dart
+++ lib/main.dart
@@ -30,23 +30,184 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    String myText = '''
+     Старую шинель, укатанную в сверток, взял Степа Векшин. Ноша освобождала
+его от  древнего обычая обмывания. К весне  1953  года на Черноморском флоте.
+''';
+
+    var myTextList = myText.split(RegExp(r"[\n ]"));
+    bool? checkboxValue = false;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Тестовая'),
+        title: Text('Тестовая ${myText.length}'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 91.0),
+                child: Divider(),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 92,
+                        width: 22,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 8,
+                            bottom: 12,
+                          ),
+                          child: RotatedBox(
+                            quarterTurns: 3,
+                            child:
+                                Text('ГЛАВНЫЕ', style: TextStyle(fontSize: 11)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 130,
+                        width: 22,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 8,
+                            top: 0,
+                          ),
+                          child: RotatedBox(
+                            quarterTurns: 3,
+                            child: Text('ДОПОЛНИТЕЛЬНЫЕ',
+                                style: TextStyle(fontSize: 11)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      height: 400,
+                      child: ReorderableListView(
+                        onReorder: (int oldIndex, int newIndex) {
+                          setState(() {
+                            print('1');
+                          });
+                        },
+                        children: [
+                          ListTile(
+                            key: ObjectKey(Text('df')),
+                            title: Text('непомерный'),
+                            leading: Checkbox(
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  checkboxValue = value;
+                                });
+                              },
+                              value: checkboxValue,
+                            ),
+                            trailing: Padding(
+                              padding: const EdgeInsets.only(
+                                right: 20,
+                              ),
+                              child: Text('1334'),
+                            ),
+                          ),
+                          ListTile(
+                            key: ObjectKey(Text('df1')),
+                            title: Text('огромный'),
+                            leading: Checkbox(
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  checkboxValue = value;
+                                });
+                              },
+                              value: checkboxValue,
+                            ),
+                            trailing: Padding(
+                              padding: const EdgeInsets.only(
+                                right: 20,
+                              ),
+                              child: Text('573'),
+                            ),
+                          ),
+                          ListTile(
+                            key: ObjectKey(Text('df2')),
+                            title: Text('невероятный, непомерный'),
+                            leading: Checkbox(
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  checkboxValue = value;
+                                });
+                              },
+                              value: checkboxValue,
+                            ),
+                            trailing: Padding(
+                              padding: const EdgeInsets.only(
+                                right: 20,
+                              ),
+                              child: Text('71'),
+                            ),
+                          ),
+                          ListTile(
+                            key: ObjectKey(Text('df3')),
+                            title: Text('колоссальный'),
+                            leading: Checkbox(
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  checkboxValue = value;
+                                });
+                              },
+                              value: checkboxValue,
+                            ),
+                            trailing: Padding(
+                              padding: const EdgeInsets.only(
+                                right: 20,
+                              ),
+                              child: Text('5'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Wrap(
+                  runSpacing: 0,
+                  spacing: 0,
+                  children: [
+                    for (var i in myTextList)
+                      InkWell(
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(4, 7, 4, 7),
+                            child: Text(
+                              i,
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            final snackBar = SnackBar(content: Text(i));
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          }),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
